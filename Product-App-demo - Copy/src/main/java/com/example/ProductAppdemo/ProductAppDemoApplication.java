2c13b07a8d927c7b065b54b37afc77b11d8cb908
msrkreddy0928 <86424537+msrkreddy0928@users.noreply.github.com>
2024-07-01 19:01:50 +0530

    Add files via upload

---
+++ Product-App-demo - Copy/src/main/java/com/example/ProductAppdemo/ProductAppDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.ProductAppdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductAppDemoApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ProductAppDemoApplication.class, args);
+	}
+
+}
---
+++ Product-App-demo - Copy/src/test/java/com/example/ProductAppdemo/ProductAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductAppdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
